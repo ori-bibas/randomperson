b62cf1bf9aa8687f8d018ece346cab021541946a
ori-bibas <oribibas6@gmail.com>
2022-06-21 14:44:54 -0400

    Coded createEmail function in RandomPersonFactory

--- src/main/java/com/example/randompersonapi/RandomPersonFactory.java
+++ src/main/java/com/example/randompersonapi/RandomPersonFactory.java
@@ -15,11 +15,22 @@ import java.util.Random;
 @Service
 public class RandomPersonFactory {
 
+    private static Random rand = new Random();
+
     public Person createRandomPerson() {
 
 
         Person obbs = new Person("Ori", "Bibas", 22, "ob@gmail.com", "4436149427", "10700 London St", "Hollywood", "FL", 32817);
         return obbs;
+    }
+
+    // createEmail() returns a string with the users email in the format example: john.doe@gmail.com. Domain and middle char is randomized.
+    public static String createEmail(String firstName, String lastName) {
+        String[] domains = {"@gmail.com", "@outlook.com", "@hotmail.com", "@aol.com", "@live.com", "@yahoo.com"};
+        Character[] chars = {'.', '-', '_'};
+        int randomDomain = rand.nextInt(domains.length);
+        int randomCharacter = rand.nextInt(chars.length);
+        return firstName.toLowerCase() + chars[randomCharacter] + lastName.toLowerCase() + domains[randomDomain];
     }
 
 }
