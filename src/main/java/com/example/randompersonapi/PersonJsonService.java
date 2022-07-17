39c7cfca3c0dadcf2a924814f86f5925e896972e
Ori Bibas <oribibas6@gmail.com>
2022-07-17 13:18:50 -0400

    added service that stores json arrays for firstnames and lastnames instead of it all in one

---
+++ src/main/java/com/example/randompersonapi/PersonJsonService.java
@@ -0,0 +1,54 @@
+package com.example.randompersonapi;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Getter
+@Setter
+@Service
+public class PersonJsonService {
+
+    private static final JSONParser jsonParser = new JSONParser();
+    private static final String firstNamesURL = "https://raw.githubusercontent.com/ori-bibas/list-of-names/main/src/first-names.json";
+    private static final String lastNamesURL = "https://raw.githubusercontent.com/ori-bibas/list-of-names/main/src/last-names.json";
+    public static JSONArray firstNames = null;
+    public static JSONArray lastNames = null;
+
+    public PersonJsonService() throws IOException, ParseException {
+        JSONObject firstNamesObj = getJSONObjectFromRemote(firstNamesURL);
+        JSONObject lastNamesObj = getJSONObjectFromRemote(lastNamesURL);
+        firstNames = (JSONArray) firstNamesObj.get("firstNames");
+        lastNames = (JSONArray) lastNamesObj.get("lastNames");
+    }
+
+    public JSONObject getJSONObjectFromRemote(String link) throws IOException, ParseException {
+
+        URL url = new URL(link);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        // Read in the response to StringBuffer
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuffer response = new StringBuffer();
+        String inputLine;
+        while((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        // Parse the response, and set a JSONArray to the array "addresses"
+        JSONObject obj = (JSONObject) jsonParser.parse(response.toString());
+
+        return obj;
+    }
+}
--- src/main/java/com/example/randompersonapi/RandomPersonFactory.java
+++ src/main/java/com/example/randompersonapi/RandomPersonFactory.java
@@ -4,6 +4,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
@@ -11,13 +12,16 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Random;
 
+import static com.example.randompersonapi.PersonJsonService.firstNames;
+import static com.example.randompersonapi.PersonJsonService.lastNames;
+
 @Service
 public class RandomPersonFactory {
 
+    @Autowired
+    private PersonJsonService personJsonService;
+
     private static final Random rand = new Random();
-    private static final JSONParser jsonParser = new JSONParser();
-    private static final String firstNamesURL = "https://raw.githubusercontent.com/ori-bibas/list-of-names/main/src/first-names.json";
-    private static final String lastNamesURL = "https://raw.githubusercontent.com/ori-bibas/list-of-names/main/src/last-names.json";
     private static final String emailsURL = "https://raw.githubusercontent.com/EthanRBrown/rrad/master/addresses-us-100.json";
 
     /*
@@ -27,14 +31,6 @@ public class RandomPersonFactory {
     public Person createRandomPerson() throws IOException, ParseException {
 
         Person randomPerson = new Person();
-
-        // Get two JSON Objects from remote repos.
-        JSONObject firstNamesObj = getJSONObjectFromRemote(firstNamesURL);
-        JSONObject lastNamesObj = getJSONObjectFromRemote(lastNamesURL);
-
-        // Create JSON Arrays for each.
-        JSONArray firstNames = (JSONArray) firstNamesObj.get("firstNames");
-        JSONArray lastNames = (JSONArray) lastNamesObj.get("lastNames");
 
         // Get random indexes to pull from.
         int randomFirstIndex = rand.nextInt(firstNames.size());
@@ -107,7 +103,7 @@ public class RandomPersonFactory {
 
         Address address = new Address();
 
-        JSONObject addresses = getJSONObjectFromRemote(emailsURL);
+        JSONObject addresses = personJsonService.getJSONObjectFromRemote(emailsURL);
         JSONArray addressesArray = (JSONArray) addresses.get("addresses");
 
         // Choose a random object from the addresses array
@@ -121,30 +117,5 @@ public class RandomPersonFactory {
         address.setZipCode((String) obj.get("postalCode"));
 
         return address;
-    }
-
-    /*
-        Written By: Ori Bibas
-            Opens a connection to my GitHub repo with plenty of data, retrieves the JSON data, parses it,
-            and returns the JSON object.
-    */
-    public JSONObject getJSONObjectFromRemote(String link) throws IOException, ParseException {
-
-        URL url = new URL(link);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        // Read in the response to StringBuffer
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuffer response = new StringBuffer();
-        String inputLine;
-        while((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        // Parse the response, and set a JSONArray to the array "addresses"
-        JSONObject obj = (JSONObject) jsonParser.parse(response.toString());
-
-        return obj;
     }
 }
